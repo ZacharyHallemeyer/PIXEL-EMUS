e4ab906a8932166ccbfde064be5c7e6ac5458710
ZacharyHallemeyer <84886285+ZacharyHallemeyer@users.noreply.github.com>
2024-04-10 14:43:43 -0700

    Add files via upload
    downloadEmus.py now iterates over dates to allow for a longer interval to download

--- downloadEmus.py
+++ downloadEmus.py
@@ -1,19 +1,28 @@
 from emm_api import emm_search_and_download
+from datetime import datetime, timedelta
 
 def download_emu_l2a_files(download_dir):
-
     instrument = "emu"
     level = "l2a"
-    start_date = "2021-02-01"
-    end_date = "2021-03-31"
+    start_date_str = "2021-02-01"
+    end_date_str = "2021-03-31"
     latest = 'true'
 
-    emm_search_and_download(download_dir=download_dir,
-                            instrument=instrument,
-                            level=level,
-                            latest=latest,
-                            start_date=start_date,
-                            end_date=end_date,
-                            use_sdc_dir_structure=True)
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+    current_date = start_date
+
+    while current_date <= end_date:
+        start_date_str = current_date.strftime("%Y-%m-%d")
+        end_date_str = (current_date + timedelta(days=1)).strftime("%Y-%m-%d")
+        emm_search_and_download(download_dir=download_dir,
+                                instrument=instrument,
+                                level=level,
+                                latest=latest,
+                                start_date=start_date_str,
+                                end_date=end_date_str,
+                                use_sdc_dir_structure=True)
+        current_date += timedelta(days=1)
+
 
 download_emu_l2a_files('../Data')
